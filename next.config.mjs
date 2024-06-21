250079214a50626479bafc386158ffa541dac2a8
Gabriel <gabrieltoscano1@outlook.com>
2024-06-21 20:55:44 -0300

    Adicionando domínios de onde as imagens estão salvas para o next confiar

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'img.favpng.com',
+          },
+          {
+            protocol: 'https',
+            hostname: 'e7.pngegg.com',
+          },
+          {
+            protocol: 'https',
+            hostname: 'www.clipartmax.com',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
